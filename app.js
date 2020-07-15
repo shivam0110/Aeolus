3222755cf57f79795be54f4824af715dbb42d5b8
Shivam <54373797+shivam0110@users.noreply.github.com>
2020-07-15 15:51:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,30 @@ window.addEventListener('load', ()=>{
     let temperatureSection = document.querySelector(".temperature")
     const temperatureSpan = document.querySelector(".temperature span");
 
+    console.log('line 65');
+    const proxy1 = "https://cors-anywhere.herokuapp.com/";
+    //const api1 = `https://api.ipgeolocation.io/ipgeo?apiKey=b104e1db249f48aeba843d10ad66469b`;
+    const api1 = `https://ipapi.co/json/`;
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+    fetch(api1)
+        .then(response =>{
+            return response.json();
+        })
+        .then(data=>{
+            console.log(data);
+                let locationTimezone = document.querySelector(".location-timezone");
+                //Setting DOM Elements from API
+                locationTimezone.textContent = data.city + ' / ' + data.region;
+                long = data.longitude;
+                lat = data.latitude;
+    });
+
+
+    
+            //long = position.coords.longitude;
+            //lat = position.coords.latitude;
             //e04cf9b52062859663ef8908c137c365
-	    //Weatherstack: 3c4e009a79904c54af52db127e8e7b87
+	        //Weatherstack: 3c4e009a79904c54af52db127e8e7b87
             
             const proxy = "https://cors-anywhere.herokuapp.com/";
             const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=e04cf9b52062859663ef8908c137c365`;
@@ -57,23 +74,4 @@ window.addEventListener('load', ()=>{
 
 
                 });
-        });
-    }
-
-});
-
-console.log('line 65');
-const proxy1 = "https://cors-anywhere.herokuapp.com/";
-//const api1 = `https://api.ipgeolocation.io/ipgeo?apiKey=b104e1db249f48aeba843d10ad66469b`;
-const api1 = `https://ipapi.co/json/`;
-
-fetch(api1)
-    .then(response =>{
-        return response.json();
-    })
-    .then(data=>{
-        console.log(data);
-            let locationTimezone = document.querySelector(".location-timezone");
-            //Setting DOM Elements from API
-            locationTimezone.textContent = data.city + ' / ' + data.region;
 });
